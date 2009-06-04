a6e25b4c5df9cc57fa22bc70507aa9a5c823a373
Nalin Dahyabhai <nalin@fedoraproject.org>
2009-06-04 22:36:59 +0000

    - put $prefix/sbin in everyone's $PATH, not just root's

--- krb5.sh
+++ krb5.sh
@@ -2,7 +2,5 @@ if ! echo ${PATH} | /bin/grep -q /usr/kerberos/bin ; then
 	PATH=/usr/kerberos/bin:${PATH}
 fi
 if ! echo ${PATH} | /bin/grep -q /usr/kerberos/sbin ; then
-	if [ `/usr/bin/id -u` = 0 ] ; then
-		PATH=/usr/kerberos/sbin:${PATH}
-	fi
+	PATH=/usr/kerberos/sbin:${PATH}
 fi
